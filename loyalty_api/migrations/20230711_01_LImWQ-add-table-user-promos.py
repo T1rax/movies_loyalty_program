4353e6603cfabdc96671b4593e36271614e4e63c
vgaksentii <vgaksentii@domclick.ru>
2023-07-11 20:40:21 +0300

    добавила таблицу user_promos

--- loyalty_api/migrations/20230711_01_LImWQ-add-table-user-promos.py
+++ loyalty_api/migrations/20230711_01_LImWQ-add-table-user-promos.py
@@ -7,5 +7,17 @@ from yoyo import step
 __depends__ = {'20230709_02_ERJiu-add-column-updated-dt', '__init__'}
 
 steps = [
-    step("")
+    step(
+        """
+        create table if not exists user_promos (
+            id uuid primary key,
+            promo_id bigint not null,
+            user_id uuid not null,
+            created_dt timestamp with time zone default now()
+        );
+        """,
+        """
+        drop table if exists user_promos;
+        """
+    )
 ]
