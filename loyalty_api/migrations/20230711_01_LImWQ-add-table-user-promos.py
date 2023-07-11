7fd4294ca6b3bb5fbb1e5b5d61fa152a8d0082ac
vgaksentii <vgaksentii@domclick.ru>
2023-07-11 11:42:59 +0300

    переимновала миграции

--- loyalty_api/migrations/20230711_01_LImWQ-add-table-user-promos.py
+++ loyalty_api/migrations/20230711_01_LImWQ-add-table-user-promos.py
@@ -4,7 +4,7 @@ add table user_promos
 
 from yoyo import step
 
-__depends__ = {'20230709_02_ERJiu-add-clumn-updated-dt', '__init__'}
+__depends__ = {'20230709_02_ERJiu-add-column-updated-dt', '__init__'}
 
 steps = [
     step("")
