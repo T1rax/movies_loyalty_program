ad138a06bd88ea539118ec4ca6ca4370078a0a68
vgaksentii <vgaksentii@domclick.ru>
2023-07-12 10:57:34 +0300

    добавила lock_timeout при дропе поля user_ids

--- loyalty_api/migrations/20230711_02_dlGwt-drop-column-user-ids.py
+++ loyalty_api/migrations/20230711_02_dlGwt-drop-column-user-ids.py
@@ -9,6 +9,7 @@ __depends__ = {'20230711_01_LImWQ-add-table-user-promos'}
 steps = [
     step(
         """
+        set lock_timeout to '5s';
         ALTER TABLE promos 
         DROP COLUMN user_ids;
         """
