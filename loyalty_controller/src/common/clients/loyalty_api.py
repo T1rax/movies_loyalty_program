a4623a511db92ca3b5578ab271bc63b6d9a612be
opodryadov <oleg_podryadov@mail.ru>
2023-07-23 23:56:36 +0500

    Правки после ревью

--- loyalty_controller/src/common/clients/loyalty_api.py
+++ loyalty_controller/src/common/clients/loyalty_api.py
@@ -16,12 +16,16 @@ class LoyaltyApiClient(AsyncClient):
     def default_headers(self) -> dict:
         return {"X-Token": self.token}
 
+    @staticmethod
+    async def is_json_successful(status) -> bool:
+        return status in (200, 201)
+
     async def get_card_info(self, user_id: str) -> dict | None:
         """Получить информацию по карте лояльности."""
 
         url = f"/api/srv/loyalty_cards/{user_id}"
         response_body = await self.get(url=url, headers=self.default_headers)
-        if response_body.status_code == 200:
+        if await self.is_json_successful(response_body.status_code):
             response = response_body.json()
             return dpath.get(response, "result", default=None)  # type: ignore
         else:
